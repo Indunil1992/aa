d8d3fdddd447cb9ceedcb7b12a748fe83cbb80d5
Indunil1992 <indunil@adroitlogic.com>
2019-09-05 17:49:58 +0530

    test http

--- aa/lambda.js
+++ aa/lambda.js
@@ -2,19 +2,17 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
-    sns.setTopicAttributes({
-        AttributeName: 'DisplayName',
-        AttributeValue: 'sachithra ',
+    sns.publish({
+        Message: 'test message',
+        Subject: 'test Subject',
+        MessageAttributes: {},
+        MessageStructure: 'String',
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:testsnsindunil'
     }).promise()
         .then(data => {
-            console.log("dataaa");
-            console.log(data);
             // your code goes here
         })
         .catch(err => {
-            console.log("errorrrr");
-            console.log(err);
             // error handling goes here
         });
 
