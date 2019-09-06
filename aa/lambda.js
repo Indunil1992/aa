593cd0500351d48100a226dc32e8968d1c782240
Indunil1992 <indunil@adroitlogic.com>
2019-09-06 14:52:39 +0530

    q

--- aa/lambda.js
+++ aa/lambda.js
@@ -4,7 +4,7 @@ const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
     sns.subscribe({
         Protocol: 'http',
-        Endpoint: 'http://testsns.com',
+        Endpoint: 'https://052p9kjn18.execute-api.us-east-1.amazonaws.com/test11/indunil/qa/sns',
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestAddTopicSubscription'
     }).promise()
         .then(data => {
