5799a65def4bb4895c4757f767321c79c605e246
Indunil1992 <indunil@adroitlogic.com>
2019-09-06 10:08:53 +0530

    sss

--- aa/lambda.js
+++ aa/lambda.js
@@ -4,7 +4,7 @@ const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
     sns.setTopicAttributes({
         AttributeName: 'Policy',
-        AttributeValue: '"Id": "135081N"',
+        AttributeValue: '"Id": "__default_policy_ID"',
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
     }).promise()
         .then(data => {
