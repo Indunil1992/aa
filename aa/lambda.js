74cdb6e65b3602c900723043b0a6f5072342e6a4
Indunil1992 <indunil@adroitlogic.com>
2019-09-06 10:24:32 +0530

    http

--- aa/lambda.js
+++ aa/lambda.js
@@ -2,21 +2,18 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 
 exports.handler = function (event, context, callback) {
-    sns.setTopicAttributes({
-        AttributeName: 'Policy',
-        AttributeValue: '"Id": "__default_policy_ID"',
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
+    sns.subscribe({
+        Protocol: 'http',
+        Endpoint: 'http://indunilRajapakse.com',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestAddTopicSubscription'
     }).promise()
         .then(data => {
-            console.log("dataaa");
-            console.log(data);
             // your code goes here
         })
         .catch(err => {
-            console.log(err);
-            console.log("errrr");
             // error handling goes here
         });
 
-    callback(null, { "message": "Successfully executed" });
+
+    callback(null, { "message": "Successfully executed:HTTP" });
 }
