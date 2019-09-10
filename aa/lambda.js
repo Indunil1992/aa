53994a385dba799cd7ef1b667974f2c9558382bd
Indunil1992 <indunil@adroitlogic.com>
2019-09-10 12:45:55 +0530

    ss

--- aa/lambda.js
+++ aa/lambda.js
@@ -4,8 +4,8 @@ const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
     sns.subscribe({
-        Protocol: 'lambda',
-        Endpoint: 'arn:aws:lambda:us-east-1:318300609668:function:lambda',
+        Protocol: 'sqs',
+        Endpoint: 'arn:aws:sqs:us-east-1:318300609668:my-queus ',
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
     }).promise()
         .then(data => {
