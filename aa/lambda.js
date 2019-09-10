e38cc0029faafef2d06ed06aabcb444eedbd56ad
Indunil1992 <indunil@adroitlogic.com>
2019-09-10 10:15:16 +0530

    k

--- aa/lambda.js
+++ aa/lambda.js
@@ -4,14 +4,18 @@ const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
     sns.subscribe({
-        Protocol: 'https',
-        Endpoint: 'https://052p9kjn18.execute-api.us-east-1.amazonaws.com/test11/indunil/qa',
+        Protocol: 'email-json',
+        Endpoint: 'indunil@adroitlogic.com',
         TopicArn: 'arn:aws:sns:us-east-1:318300609668:TestSNS'
     }).promise()
         .then(data => {
+            console.log("dataaaa");
+              console.log(data);
             // your code goes here
         })
         .catch(err => {
+              console.log("fail");
+                console.log(err);
             // error handling goes here
         });
 
